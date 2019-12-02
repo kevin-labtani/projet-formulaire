63f2a7079cd06637f873f6cf7af2bbaef24284b4
Loïc Lissens <loiclissens@gmail.com>
2019-12-02 10:56:48 +0100

    switched branch

--- index.php
+++ index.php
@@ -12,9 +12,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-        <h1>Hello World</h1>
-    
-
+        <header>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo center">Hacker Poulette</a>
+                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        <li><a href="#">Nos produits</a></li>
+                        <li><a href="#">Nous contacter</a></li>
+                    </ul>
+                </div>
+            </nav>
+         </header>
+        <main>
+        </main>
+        <footer>
+        </footer>
         <!--JavaScript at end of body for optimized loading-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script>
